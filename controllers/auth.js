cddaf1f4b7571393418b1d1acd07fde1e91620fc
Panchimaal <panchimaal@hotmail.com>
2022-08-01 16:48:35 -0500

    Add files via upload

---
+++ controllers/auth.js
@@ -0,0 +1,20 @@
+const User = require('../models/User')
+const {StatusCodes} = require('http-status-codes')
+const { BadRequestError } = require('../errors')
+const {BadRequestError} = require('../errors')
+const register = async (req, res) => {
+    const {name,email,password} = req.body
+    if(!name || !email || !password) {
+throw new BadRequestError('Plese provide name, email and password')
+    }
+    const user = await User.create({...req.body})
+    res.status(StatusCodes.CREATED).json({ user })
+}
+const login = async (req, res) => {
+    res.send('login user')
+}
+
+module.exports = {
+    register,
+    login,
+}
